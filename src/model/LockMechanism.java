e58bca96b571b5fd1fbaa0c0abab6dbe9e79a6a7
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2026-05-19 13:55:58 +0700

    Add files via upload

---
+++ src/model/LockMechanism.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.enums;
+
+public enum LockMechanism {
+    NO_LOCK,
+    SYNCHRONIZED,
+    FILE_LOCK,
+    OPTIMISTIC_LOCK
+}

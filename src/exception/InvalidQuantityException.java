628976a8ba33f62b73bc68263f85bb63060e5bba
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2026-06-04 12:59:41 +0700

    Add InvalidQuantityException class for error handling

---
+++ src/exception/InvalidQuantityException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class InvalidQuantityException
+        extends FlashSaleException {
+
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
+}

46e1a58f5fb08204e0c04dd2a89f945b002e7d5c
pthaok6 <phamthaotr0211@gmail.com>
2026-06-04 17:35:18 +0700

    Update OrderRepository.java

--- src/repository/OrderRepository.java
+++ src/repository/OrderRepository.java
@@ -1,6 +1,11 @@
 package repository;
 
+import java.util.List;
+
 import model.Entity.Order;
+import model.Entity.OrderDetail;
+import java.util.ArrayList;
+
 
 
 public class OrderRepository extends CsvRepository<Order> {
@@ -22,6 +27,61 @@ public class OrderRepository extends CsvRepository<Order> {
             return null;
         }
     }
+
+    public int getPurchasedQuantity(String customerId,
+                                String flashItemId) {
+
+    int total = 0;
+
+    try {
+
+        List<Order> orders = findAll();
+
+        OrderDetailRepository detailRepo =
+                new OrderDetailRepository();
+
+        for (Order order : orders) {
+
+            if (!customerId.equals(
+                    order.getCustomerId())) {
+
+                continue;
+            }
+
+            // List<OrderDetail> details =
+            //         detailRepo.findByOrderId(
+            //                 order.getId()
+            //         );
+            List<OrderDetail> details = new ArrayList<>();
+                for (OrderDetail detail : detailRepo.findAll()) {
+    
+                    if (order.getId().equals(
+                            detail.getOrderId())) {
+    
+                        details.add(detail);
+                    }
+                }
+            
+
+            for (OrderDetail detail : details) {
+
+                if (flashItemId.equals(
+                        detail.getFlashItemId())) {
+
+                    total += detail.getQuantity();
+                }
+            }
+        }
+
+    } catch (Exception e) {
+
+        System.out.println(
+                "[ERROR] " + e.getMessage()
+        );
+    }
+
+    return total;
+}
 }
     
 

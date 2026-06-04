5b29f22eb00a7d797ba6a4b90f205d495b18ccb9
pthaok6 <phamthaotr0211@gmail.com>
2026-06-04 17:35:20 +0700

    Update OrderDetailRepository.java

--- src/repository/OrderDetailRepository.java
+++ src/repository/OrderDetailRepository.java
@@ -1,27 +1,72 @@
 package repository;
 
-import model.Entity.Order;
+import model.Entity.OrderDetail;
 
 
-public class OrderRepository extends CsvRepository<Order> {
+public class OrderDetailRepository extends CsvRepository<OrderDetail> {
 
-    public OrderRepository() {
-        super("data/orders.csv");
+    public OrderDetailRepository() {
+        super("data/order_details.csv");
     }
     // --- Quản lý Event ---
 
     @Override
-    protected Order mapFromCsv(String csvLine) {
+    protected OrderDetail mapFromCsv(String csvLine) {
 
-        Order order = new Order();
+        OrderDetail orderDetail = new OrderDetail();
 
         try {
-            order.fromCsvLine(csvLine);
-            return order;
+            orderDetail.fromCsvLine(csvLine);
+            return orderDetail;
         } catch (Exception e) {
             return null;
         }
     }
+
+//     public int getPurchasedQuantity(String customerId,
+//                                 String flashItemId) {
+
+//     int total = 0;
+
+//     try {
+
+//         List<Order> orders = findAll();
+
+//         OrderDetailRepository detailRepo =
+//                 new OrderDetailRepository();
+
+//         for (Order order : orders) {
+
+//             if (!customerId.equals(
+//                     order.getCustomerId())) {
+
+//                 continue;
+//             }
+
+//             List<OrderDetail> details =
+//                     detailRepo.findByOrderId(
+//                             order.getId()
+//                     );
+
+//             for (OrderDetail detail : details) {
+
+//                 if (flashItemId.equals(
+//                         detail.getFlashSaleItemId())) {
+
+//                     total += detail.getQuantity();
+//                 }
+//             }
+//         }
+
+//     } catch (Exception e) {
+
+//         System.out.println(
+//                 "[ERROR] " + e.getMessage()
+//         );
+//     }
+
+//     return total;
+// }
 }
     
 
